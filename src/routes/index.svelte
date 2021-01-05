f2d648e1b0207bcdf15b0a4e88309b1d7fc5a86e
Jeremy Bunting <buntingj@gmail.com>
2021-01-04 21:31:20 -0500

    adding netlify cms

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,18 @@
 <script>
 	import successkid from 'images/successkid.jpg';
+	import { onMount } from 'svelte';
+
+  onMount(() => {
+    if (window.netlifyIdentity) {
+      window.netlifyIdentity.on("init", user => {
+        if (!user) {
+          window.netlifyIdentity.on("login", () => {
+            document.location.href = "/admin/";
+          });
+        }
+      });
+    }
+  });
 </script>
 
 <style>
@@ -38,6 +51,7 @@
 
 <svelte:head>
 	<title>Sapper project template</title>
+	<script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
 </svelte:head>
 
 <h1>Great success!</h1>
